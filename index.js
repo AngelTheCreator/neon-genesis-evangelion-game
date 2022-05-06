d3feeb307376a45257782b13703eefaead8d3466
AngelTheCreator <angel_guevara94@hotmail.com>
2022-05-05 20:40:02 -0700

    third commit

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
 
     draw(){
@@ -141,6 +142,36 @@ function rectangularCollision({rectangle1,rectangle2}){
         && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height  
     )
 }
+
+function determineWinner({player, enemy, timerId}) {
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if (player.health === enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    } else if (player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+    } else if (enemy.health > player.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+    }
+}
+
+
+
+let timer = 60
+let timerId
+function decreaseTimer(){
+    if(timer > 0){
+        timerId = setTimeout(decreaseTimer,1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+    
+    if(timer === 0) {
+        document.querySelector('#displayText').style.display = 'flex'
+        determineWinner({player, enemy})
+    }    
+}
+decreaseTimer();
 //Function to animate the characters on screen
 function animate() {
     window.requestAnimationFrame(animate)
@@ -172,7 +203,8 @@ function animate() {
 
      ) {
          player.isAttacking = false;
-        console.log('player attack successful');
+         enemy.health -= 5
+         document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
     //Collision detection for Enemy
     if(
@@ -184,7 +216,12 @@ function animate() {
 
      ) {
          enemy.isAttacking = false;
-        console.log('enemy attack successful');
+         player.health -= 5
+         document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+    // end game based on health
+    if(enemy.health <= 0 || player.health <= 0) {
+        determineWinner({player, enemy, timerId})
     }
 }
 
